c7e16a63ebdafe30d944516efbdbeac680eae698
rogergiliclota <rogergili2015@gmail.com>
2020-04-16 21:01:24 +0200

    footer

--- src/components/common/footer.tsx
+++ src/components/common/footer.tsx
@@ -83,11 +83,9 @@ const poweredBySVG = () => {
 
 export const Footer: React.FC<Props> = props => {
     return (
-        <!--FooterWrapper title={GIT_COMMIT} {...props}>
-            <a href="https://0x.org/" target="_blank" rel="noopener noreferrer">
-                {poweredBySVG()}
-            </a>
-        </FooterWrapper-->
-        © Roger Gili 2020
+        <FooterWrapper>
+            ©Roger Gili 2020
+        </FooterWrapper>
+
     );
 };
